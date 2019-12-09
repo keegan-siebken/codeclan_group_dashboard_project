8a9badf57607561ca52248aa0abe0151af8f4967
Nag33k <keegan.siebken@gmail.com>
2019-12-09 14:37:52 +0000

    added basic API call code to global file

--- codeclan_ga_data/global.R
+++ codeclan_ga_data/global.R
@@ -2,3 +2,21 @@
 library(googleAnalyticsR)
 library(keyring)
 
+# Setting options(googleAuthR.client_id) and options(googleAuthR.client_secret) to activate API call
+options(googleAuthR.client_id = keyring::key_get(service = "ga_client_id", keyring = "googleanalytics"))
+options(googleAuthR.client_secret = keyring::key_get(service = "ga_client_secret", keyring = "googleanalytics"))
+
+# reloading package
+devtools::reload(pkg = devtools::inst("googleAnalyticsR"))
+
+#Authenticating account
+ga_auth()
+
+#Select all CodeClan website data
+my_ga_id <- 102407343
+
+#Call the API to access the data you require
+google_analytics(my_ga_id, 
+                 date_range = c("2019-01-01", "2019-03-01"), 
+                 metrics = "sessions", 
+                 dimensions = "date")
